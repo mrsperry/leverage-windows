0f54be40a273183f4315e03a93618322d3ee9277
Josh <mrjoshuasperry@gmail.com>
2021-02-13 09:01:10 -0500

    Push modal creation to its own function for about page

--- src/js/about.js
+++ src/js/about.js
@@ -13,29 +13,30 @@
 
         $(brand)
             .removeAttr("href")
-            .on("click", () => {
-                const modal = $("<div>")
-                    .addClass("modal")
-                    .hide()
-                    .fadeIn()
-                    .appendTo(document.body);
-                
-                const closeModal = () => modal.fadeOut(400, () => modal.remove());
-
-                $("<div>")
-                    .addClass("background")
-                    .on("click", () => closeModal())
-                    .appendTo(modal);
-                
-                const content = $("<div>")
-                    .addClass("content")
-                    .appendTo(modal);
-                $("<button>")
-                    .text("X")
-                    .on("click", () => closeModal())
-                    .appendTo(content);
-                $(clone)
-                    .appendTo(content);
-            });
+            .on("click", () => $(clone).appendTo(createModal("brand")));
     }
 })();
+
+const createModal = clazz => {
+    const modal = $("<div>")
+        .addClass("modal")
+        .hide()
+        .fadeIn()
+        .appendTo(document.body);
+    const closeModal = () => modal.fadeOut(400, () => modal.remove());
+
+    $("<div>")
+        .addClass("background")
+        .on("click", closeModal)
+        .appendTo(modal);
+
+    const content = $("<div>")
+        .addClass("content " + (clazz === undefined ? "" : clazz))
+        .appendTo(modal);
+    $("<button>")
+        .text("X")
+        .on("click", closeModal)
+        .appendTo(content);
+
+    return content;
+};
