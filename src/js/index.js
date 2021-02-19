b993b10a05a4f244a86e0100e702ef35127a498f
Josh <mrjoshuasperry@gmail.com>
2021-02-18 21:36:09 -0500

    Add an additional video to the home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 // Play next slide when video ends
-$("#video").on("ended", () => $(".carousel").carousel("next"));
+$("video").on("ended", () => $(".carousel").carousel("next"));
 
 // Restart video when selected
 $(".carousel").on("slide.bs.carousel", args => {
-    if (args.to === 0) {
-        const video = $("#video").get(0);
+    if (args.to === 0 || args.to === 4) {
+        const video = $("#video-" + (args.to === 0 ? 1 : 2)).get(0);
         video.currentTime = 0;
         video.play();
     }
